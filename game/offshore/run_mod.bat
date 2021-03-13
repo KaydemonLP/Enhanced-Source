ec31d8c39fda616996597caff6cb7b338f86f63b
KaydemonLP <kaidemonlp@gmail.com>
2021-03-13 12:55:43 +0100

    Initial Bins, Scripts and Resource commit

---
+++ game/offshore/run_mod.bat
@@ -0,0 +1,1 @@
+@start "source" "../swarm.exe" -game offshore -override_vpk -w  1600 -h 900 -windowed -noborder -dev +map movement_test
---
+++ game/offshore/run_tools.bat
@@ -0,0 +1,1 @@
+@start "source" "../swarm.exe" -dev -allowdebug -game offshore -override_vpk -w  1600 -h 900 -windowed -noborder -tools -nop4
