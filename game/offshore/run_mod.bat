ebadc8b1020a12e91dcb8bc72c4decee62226c99
KaydemonLP <kaidemonlp@gmail.com>
2021-04-29 23:45:43 +0200

    Some select game files that are required

--- game/offshore/run_mod.bat
+++ game/offshore/run_mod.bat
@@ -1,1 +1,1 @@
-@start "source" "../swarm.exe" -game offshore -override_vpk -w  1600 -h 900 -windowed -noborder -dev +map movement_test
+@start "source" "E:\Moding games\steamapps\common\Portal 2\portal2.exe" -game "E:\Making gaems\Offshore\game\offshore" -override_vpk -w  1600 -h 900 -windowed -noborder -dev +map movement_test
